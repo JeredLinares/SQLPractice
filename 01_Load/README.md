7a56b3b37065a68b749a78f392765490968973fa
JD Linares <JeredL@gmail.com>
2025-01-13 17:34:32 +0000

    Added employees section to load

---
+++ 01_Load/README.md
@@ -0,0 +1,10 @@
+# Load Data
+JD Linares 
+Created: 2025 01 12
+
+## Databases
+- CMS: All custom loading 
+- SEC: All custom loading
+- Employees: Provided by MySQL
+
+
--- 01_Load/load_2023_q4.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA INFILE '/data/nh_mds/Facility_Level_MDS_Frequency_Q4_2023.csv' 
-INTO TABLE 2023_q4 
-FIELDS terminated by ',' 
-enclosed by '"' 
-lines terminated by '\n' 
-ignore 1 rows
