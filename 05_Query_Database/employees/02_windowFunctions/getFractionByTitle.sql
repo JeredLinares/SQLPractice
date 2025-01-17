c9e9e4c9df3f65ef0c99595f936d86544de54671
JD Linares <JeredL@gmail.com>
2025-01-17 19:48:25 +0000

    practicing

--- 05_Query_Database/employees/02_windowFunctions/getFractionByTitle.sql
+++ 05_Query_Database/employees/02_windowFunctions/getFractionByTitle.sql
@@ -1,8 +1,8 @@
-/*
 -- Set DB
 USE employees;
 
 
+/*
 -- Check for nulls in to_date of titles
 SELECT 
 	sum(isnull(to_date))
@@ -63,7 +63,6 @@ WHERE
 ;
 
 
-*/
 
 
 -- Join titles with employees
@@ -71,31 +70,153 @@ WHERE
 -- number distict emp_no: 300,024
 -- number active titles: 240,124
 -- number of old to_date: 59,900	#NOTE: nuse max(to_date)
--- Hangs: SELECT emp_no, max(to_date) OVER() from titles;
 
-SELECT 
+WITH cte as (
+	SELECT 
+		emp_no,
+		title,
+		from_date,
+		to_date,
+		MAX(from_date) OVER (PARTITION BY emp_no) as last_from ,
+		MAX(to_date) OVER(PARTITION BY emp_no) as last_to 
+	FROM 
+		titles 
+    )
+SELECT
 	emp_no,
 	title,
 	from_date,
-	MAX(from_date) OVER(PARTITION BY emp_no) as last_from_day
-	MAX(to_date) OVER(PARTITION BY emp_no) as last_to_day
+	to_date
 FROM 
-	titles
-JOIN
+	cte
+JOIN 
 	employees
 ON
-	employees.emp_no = titles.emp_no
-HAVING
-	MAX(to_date) > NOW()
-GROUP BY
-	emp_no
+	employees.emp_no = cte.emp_no
+WHERE
+	from_date=last_from
+	AND
+	to_date=last_to
 ;
 
--- Get Fraction by title
-/*
-SELECT 
-	sum(*) over() as total employees
-
-FROM 
-	employees
 */
+
+-- Condense cte
+/*
+WITH cte as (
+	SELECT 
+		emp_no,
+		title,
+		from_date,
+		to_date,
+		MAX(from_date) OVER (PARTITION BY emp_no) as last_from ,
+		MAX(to_date) OVER(PARTITION BY emp_no) as last_to 
+	FROM 
+		titles 
+    )
+SELECT
+	emp_no,
+	title,
+	from_date,
+	to_date
+FROM 
+	cte
+WHERE
+	from_date=last_from
+	AND
+	to_date=last_to
+;
+*/
+
+/*
+WITH cte as (
+	SELECT 
+		emp_no,
+		title,
+		from_date,
+		to_date,
+		MAX(from_date) OVER (PARTITION BY emp_no) as last_from ,
+		MAX(to_date) OVER(PARTITION BY emp_no) as last_to
+	FROM 
+		titles 
+    )
+SELECT
+	title,
+	count(emp_no) OVER ( PARTITION BY title) AS num_title,
+	count(emp_no) OVER () as all_emp,
+	(count(emp_no) OVER ( PARTITION BY title) / count(emp_no) OVER ()) AS frac_title
+FROM 
+	cte
+WHERE
+	from_date=last_from
+	AND
+	to_date=last_to
+GROUP BY 
+	title
+;
+
+
+
+-- count employees in each title
+WITH cte as (
+	SELECT 
+		emp_no,
+		title,
+		from_date,
+		to_date,
+		MAX(from_date) OVER (PARTITION BY emp_no) as last_from ,
+		MAX(to_date) OVER(PARTITION BY emp_no) as last_to
+	FROM 
+		titles 
+    )
+SELECT
+	title,
+	emp_no,
+	count(emp_no) OVER (PARTITION BY title),
+	count(emp_no) OVER (),
+	round(count(emp_no) OVER (PARTITION BY title) / count(emp_no) OVER (),2)
+FROM 
+	cte
+WHERE
+	to_date=last_to
+LIMIT 
+	1
+;
+*/
+
+
+-- Get Fraction for each title
+WITH cte2 as (
+	WITH cte1 as (
+		SELECT 
+			emp_no,
+			title,
+			from_date,
+			to_date,
+			MAX(from_date) OVER (PARTITION BY emp_no) as last_from ,
+			MAX(to_date) OVER(PARTITION BY emp_no) as last_to
+		FROM 
+			titles 
+	    )
+	SELECT
+		title,
+		emp_no,
+		count(emp_no) OVER (PARTITION BY title),
+		count(emp_no) OVER (),
+		round(count(emp_no) OVER (PARTITION BY title) / count(emp_no) OVER (),2) as title_fract
+	FROM 
+		cte1
+	WHERE
+		to_date=last_to
+    )
+Select 
+    	title,
+	title_fract
+FROM 
+	cte2
+GROUP BY 
+	title
+;
+
+
+
